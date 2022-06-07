5d3582b55f201ea535b2b4af8fbe51e01998d951
Claire Kim <kclaire56@yahoo.com>
2022-06-07 11:07:47 -0700

    add db

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-from fastapi  import FastAPI
+from fastapi  import FastAPI , Header, HTTPException
+from pydantic import BaseModel
 
+fake_secret_token = "nabagereka"
 app = FastAPI()
+
+fake_db = {
+    "foo": {"id": "foo", "title": "Foo", "description": "There goes my hero"},
+    "bar": {"id": "bar", "title": "Bar", "description": "The bartenders"},
+}
+
+class Item(BaseModel):
+    id: str
+    title: str
+    description: Union[str, None] = None
 
 @app.get("/")
 async def root():
     return {"message": "My FastAPI world"}
+
+@app.get("/home")
+async def root():
+    return {"This is a home"}
+
