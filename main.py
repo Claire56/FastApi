a1be4756a7dc06ef2d88dc170f65a111fa4f0c99
Claire Kim <kclaire56@yahoo.com>
2022-06-07 10:23:14 -0700

    added missing decorator

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from fastapi  import FastAPI
 
 app = FastAPI()
 
-app.get('/')
+@app.get("/")
 async def root():
-    return {'messege': 'My FastAPI world'}
+    return {"message": "My FastAPI world"}
